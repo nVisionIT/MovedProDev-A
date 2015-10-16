fad8f360287221cb76876d77b0069d37b9a58de8
Giulz00 <g.pizzocaro00@gmail.com>
2015-10-16 14:37:41 +0200

    Added unit tests

--- Pro.Dev.A.Tests/UnitTest1.cs
+++ Pro.Dev.A.Tests/UnitTest1.cs
@@ -1,14 +1,61 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pro.Dev.A.Rest;
 
 namespace Pro.Dev.A.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class ValuesControllerTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        ValuesController1 con;
+
+        [TestInitialize]
+        public void Setup()
         {
+            con = new ValuesController1();
         }
+    
+        [TestMethod]
+        public void SendJson_Success()
+        {
+            EmailModel email = new EmailModel();
+
+            email.To = "me@foo.com";
+            email.Subject = "me@foo.com";
+            email.Message = "me@foo.com";
+            email.DeliveryType = "me@foo.com";
+
+            con.Submit(email);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void SendJson_Null()
+        {
+            EmailModel email = new EmailModel();
+
+            email.To = null;
+            email.Subject = null;   
+            email.Message = "me@foo.com";
+            email.DeliveryType = "me@foo.com";
+
+            con.Submit(email);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void SendJson_StringEmpty()
+        {
+            EmailModel email = new EmailModel();
+
+            email.To = string.Empty;
+            email.Subject = string.Empty;
+            email.Message = "me@foo.com";
+            email.DeliveryType = "me@foo.com";
+
+            con.Submit(email);
+        }
+
+
     }
 }
